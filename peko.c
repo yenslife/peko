0dc160b9435937f67f907cfa86f1f4a1668d1fe7
海狸大師 <77geo5rge6@gmail.com>
2022-05-27 13:42:12 +0800

    peko

---
+++ peko.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+#define peko int
+#define pekO main
+#define peKo (
+#define peKO )
+#define pEko {
+#define pEkO }
+#define pEKo printf
+#define pEKO ;
+#define Peko return
+#define PeKO 0
+
+peko pekO peKo peKO pEko
+    pEKo peKo "PEKO" peKO pEKO
+    Peko PeKO pEKO
+pEkO
