71d3f712c9a2c855de67274130d4e3f3800fb16f
yenslife <77geo5rge6@gmail.com>
2024-04-14 21:20:51 +0800

    Add header file

--- peko.c
+++ peko.c
@@ -1,16 +1,4 @@
-#include<stdio.h>
-
-#define peko int
-#define pekO main
-#define peKo (
-#define peKO )
-#define pEko {
-#define pEkO }
-#define pEKo printf
-#define pEKO ;
-#define Peko return
-#define PeKO 0
-
+#include "peko.h"
 peko pekO peKo peKO pEko
     pEKo peKo "PEKO" peKO pEKO
     Peko PeKO pEKO
---
+++ peko.h
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+#define peko int
+#define pekO main
+#define peKo (
+#define peKO )
+#define pEko {
+#define pEkO }
+#define pEKo printf
+#define pEKO ;
+#define Peko return
+#define PeKO 0
